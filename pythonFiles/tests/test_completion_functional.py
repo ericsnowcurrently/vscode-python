817309f8fbd3273c4b4672459b7368cb78e3059f
Eric Snow <ericsnowcurrently@gmail.com>
2019-06-25 11:17:46 -0600

    Frame out the functional tests.

---
+++ pythonFiles/tests/test_completion_functional.py
@@ -0,0 +1,11 @@
+import unittest
+
+from completion import main
+
+
+class ScriptTests(unittest.TestCase):
+
+    @unittest.expectedFailure
+    def test_typical(self):
+        raise NotImplementedError
+        main(jediPath, jediPreview, modulesToLoad)
