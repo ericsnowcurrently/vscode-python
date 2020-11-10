e56bf8c660a2c72e1b987bc74e121f2e46249e12
Eric Snow <ericsnowcurrently@gmail.com>
2020-11-10 13:59:11 -0700

    Add Python build helpers.

--- src/client/pythonEnvironments/base/info/pythonBuild.ts
+++ src/client/pythonEnvironments/base/info/pythonBuild.ts
@@ -2,9 +2,46 @@
 // Licensed under the MIT License.
 
 import { Architecture } from '../../../common/utils/platform';
-import { mergeVersions } from './pythonVersion';
+import { copyVersion, mergeVersions, normalizeVersion, validateVersion } from './pythonVersion';
 
-import { PythonBuildInfo } from '.';
+import { PythonBuildInfo, PythonVersion } from '.';
+
+/**
+ * Make an as-is (deep) copy of the given info.
+ */
+export function copyBuild(info: PythonBuildInfo): PythonBuildInfo {
+    const copied: PythonBuildInfo = { ...info };
+    copied.version = copyVersion(copied.version);
+    return copied;
+}
+
+/**
+ * Make a copy and set all the properties properly.
+ */
+export function normalizeBuild(info: PythonBuildInfo): PythonBuildInfo {
+    const norm = { ...info };
+    if (!norm.version) {
+        // tslint:disable-next-line:no-object-literal-type-assertion
+        norm.version = {} as PythonVersion;
+    }
+    norm.version = normalizeVersion(norm.version);
+    if (!norm.arch) {
+        norm.arch = Architecture.Unknown;
+    }
+    return norm;
+}
+
+/**
+ * Fail if any properties are not set properly.
+ *
+ * Optional properties that are not set are ignored.
+ *
+ * This assumes that the info has already been normalized.
+ */
+export function validateBuild(info: PythonBuildInfo) {
+    validateVersion(info.version);
+    // There's nothing to do for info.arch.
+}
 
 /**
  * Make a copy of "build" and fill in empty properties using "other."
